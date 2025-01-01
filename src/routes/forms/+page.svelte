50b0081d1e8a25ff2a9ef01c62cff09fdc5f7ecb
Sergey Mochalov <proweb@users.noreply.github.com>
2025-01-01 23:54:51 +0300

    new page

---
+++ src/routes/forms/+page.svelte
@@ -0,0 +1,8 @@
+<svelte:head>
+	<title>Формы в Sveltekit</title>
+	<meta name="description" content="Обработка форм" />
+</svelte:head>
+
+<section class="uk-section">
+	<h1>Формы в Sveltekit</h1>	 
+</section>
